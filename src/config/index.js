5fd0265332dd4705d559172154ac231508cd9627
Marcelo Brosowicz de Paulo <marcelo.brosowicz@signaedu.com>
2019-04-29 17:00:46 -0300

    First blood

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+import languages from "./langs";
+
+const swapi = {
+  searchPath: "?search=",
+  baseUrl: "https://swapi.co/api/",
+  filmsPath: "films/",
+  peoplePath: "people/",
+  planetsPath: "planets/",
+  speciesPath: "species/",
+  starshipsPath: "starships/",
+  vehiclesPath: "vehicles/"
+};
+
+export { languages, swapi };
---
+++ src/config/langs.js
@@ -0,0 +1,5 @@
+import languages from "@cospired/i18n-iso-languages";
+
+languages.registerLocale(require("@cospired/i18n-iso-languages/langs/pt.json"));
+
+export default languages;
